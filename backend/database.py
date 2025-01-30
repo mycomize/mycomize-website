ab4a7d1f38a747494bd5f3b1bd1e30286d28caea
Connor Davis <connojdavis@gmail.com>
2025-01-30 15:13:59 -0700

    Add database logic and BTCPay checkout

---
+++ backend/database.py
@@ -0,0 +1,31 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+
+DATABASE_URL = "sqlite:///./invoices.db"
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+class Invoice(Base):
+    __tablename__ = "invoices"
+
+    email = Column(String, primary_key=True)
+    payment_type = Column(String, nullable=False)
+    btcpay_invoice_id = Column(String, nullable=True, index=True)
+    btcpay_invoice_status = Column(String, nullable=True)
+    
+Base.metadata.create_all(bind=engine)
+
+# DB init
+def get_invoice_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
+def dump_invoice_db(db: Session):
+    invoices = db.query(Invoice).all()
+    print([invoice.__dict__ for invoice in invoices])
--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,29 @@
+import httpx
 import json
 import os
 import stripe
 
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, Depends, HTTPException, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
 from pydantic import BaseModel
+from sqlalchemy.orm import Session
+
+from database import Invoice, get_invoice_db, dump_invoice_db
+
 
 app = FastAPI()
 stripe_price_id = ''
 sah_price = 0.0
 tax_rate = 0.0
 tax = 0.0
+btcpay_api_key = ''
+
+FRONTEND_URL = "http://localhost:5173"
 
 # NOTE: be mindful of the protocol (http/https) and port number
 origins = [
-    "http://localhost:5173"
+    FRONTEND_URL
 ]
 
 # Add CORS middleware to allow requests from your frontend origin
@@ -30,6 +38,9 @@ app.add_middleware(
 with open("config.json", 'r') as f:
     config = json.load(f)
     
+    btcpay_url = config['btcpay_url']
+    btcpay_store_id = config['btcpay_store_id']
+    btcpay_api_key = config['btcpay_api_key']
     stripe.api_key = config['stripe_secret_key']
     stripe_price_id = config['stripe_price_id']
     sah_price = config['price']
@@ -38,18 +49,76 @@ with open("config.json", 'r') as f:
     
     print(f"Loaded config: stripe_price_id={stripe_price_id} sah_price={sah_price} tax_rate={tax_rate} tax={tax}")
 
-
 #
 # API Endpoints
 #
+async def create_btcpay_invoice(customer_email):
+    url = f"{btcpay_url}/api/v1/stores/{btcpay_store_id}/invoices"
+
+    headers = {
+        "Authorization": f"token {btcpay_api_key}",
+        "Content-Type": "application/json"
+    }
+
+    data = {
+        "metadata": {
+            "buyerEmail": customer_email,
+            "itemDesc": "shroomsathome",
+            "taxIncluded": tax,
+            "posData": {
+               "sub_total": sah_price,
+               "total": sah_price + tax 
+            }
+        },
+        "checkout": {
+            "speedPolicy": "MediumSpeed", # 1 confirmation
+            "paymentMethods": ["BTC", "BTC-LightningNetwork"],
+            "redirectURL": FRONTEND_URL + "/pay-submitted?type=btc",
+            "redirectAutomatically": True,
+        },
+        "amount": str(round(sah_price + tax, 2)),
+        "currency": "USD"
+    }
+    
+    async with httpx.AsyncClient() as client:
+        response = await client.post(url, headers=headers, json=data)
+        
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise HTTPException(status_code=response.status_code, detail=response.text)
+
 
 @app.post("/checkout")
-async def checkout(request: Request):
+async def checkout(request: Request, db: Session = Depends(get_invoice_db)):
     body = await request.json()
     payment_type = body['type']
     customer_email = body['email']
     
     print(f"POST: /checkout: payment_type={payment_type} customer_email={customer_email}")
+    
+    if payment_type == 'btc':
+        try:
+            invoice = await create_btcpay_invoice(customer_email)
+            invoice_id = invoice["id"]
+            invoice_status = invoice["status"]
+            
+            db_entry = Invoice(email=customer_email,
+                               payment_type=payment_type,
+                               btcpay_invoice_id=invoice_id,
+                               btcpay_invoice_status=invoice_status)
+
+            db.add(db_entry)
+            db.commit()
+            db.refresh(db_entry)
+            
+            dump_invoice_db(db)
+            
+            return { "checkoutLink": invoice["checkoutLink"] }
+        except:
+            print("Failed to create invoice")
+        
+
 
 @app.get("/price")
 def price():
