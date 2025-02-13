7ea3f2b7ac0b300197716087a46799f14219ec8c
Connor Davis <connojdavis@gmail.com>
2025-02-13 04:07:36 +0000

    Add initial stripe checkout success

--- frontend/src/OrderStatus.jsx
+++ frontend/src/OrderStatus.jsx
@@ -7,7 +7,6 @@ import Divider from "./Divider";
 
 async function getInvoice(invoice_id) {
     try {
-        // TODO: move body into query parameter
         const url = import.meta.env.VITE_BACKEND_URL + `/invoice?invoice_id=${invoice_id}`;
         const response = await fetch(url, { method: "GET" });
         
@@ -26,14 +25,16 @@ async function getInvoice(invoice_id) {
 
 export function OrderStatus() {
     const [invoiceState, setInvoiceState] = useState("");
+    const [invoiceId, setInvoiceId] = useState("");
     const [searchParams, setSearchParams] = useSearchParams();
 
     const type = searchParams.get("type");
-    const clientInvoiceId = searchParams.get("invoice_id");
 
     useEffect(() => {
         if (type === "btc") {
-            const url = import.meta.env.VITE_BACKEND_URL + `/btcpay-webhook-events?invoice_id=${clientInvoiceId}`
+            setInvoiceId(searchParams.get("invoice_id"));
+
+            const url = import.meta.env.VITE_BACKEND_URL + `/btcpay-webhook-events?invoice_id=${invoiceId}`
             const eventSource = new EventSource(url);
             
             eventSource.onmessage = (event) => {
@@ -50,13 +51,21 @@ export function OrderStatus() {
             return () => {
                 eventSource.close();
             };
+        } else if (type === "stripe") {
+            setInvoiceId(searchParams.get("session_id"));
+            
+            // implement webhhok / payment status check
+            
         }
     }, []);
     
-    if (type == "btc") {
+    if (type === "btc") {
         // If state is Settled, send an email with the ePub
-        return <OrderStatusBTC state={invoiceState} invoice_id={clientInvoiceId} />    
-    } 
+        return <OrderStatusBTC state={invoiceState} invoice_id={invoiceId} />    
+    } else if (type === "stripe") {
+        // TODO: implement state update for stripe
+        return <OrderStatusStripe state={invoiceState} invoice_id={invoiceId} />
+    }
 }
 
 function OrderStatusBTC(props) {
@@ -73,3 +82,20 @@ function OrderStatusBTC(props) {
         </div>
     );
 }
+
+function OrderStatusStripe(props) {
+    return (
+        <div className="flex flex-col mx-auto px-4 max-w-prose gap-4 h-screen text-slate-200 text-xs m-6">
+            <Header />
+            <Divider />
+            <h1>Order Status: {props.state}</h1>
+            <p>
+                Once payment has settled we will send the guide to the email you provided. 
+                Your invoice ID is {props.invoice_id}. Please keep it for your records.
+            </p>
+            <Footer />
+        </div>
+    );
+}
+
+
